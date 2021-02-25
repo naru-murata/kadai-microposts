542aec6fa169fe8a2d4edf393ed8e200a6998a42
naru.murata <ubuntu@ip-172-31-20-62.ap-northeast-1.compute.internal>
2021-02-25 06:25:22 +0000

    follow, unfollow

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,14 +1,8 @@
 class ToppagesController < ApplicationController
   def index
     if logged_in?
-      @micropost = current_user.microposts.build
+      @micropost = current_user.microposts.build  # form_with 用
       @microposts = current_user.microposts.order(id: :desc).page(params[:page])
     end
-  end
-  
-  def new
-  end
-  
-  def edit
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
@@ -16,19 +17,31 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    
+
     if @user.save
-      flash[:success] = '正常にユーザー登録がされました'
+      flash[:success] = 'ユーザを登録しました。'
       redirect_to @user
     else
-      flash.now[:danger] = 'ユーザー登録できませんでした'
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
     end
   end
-private
+
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
   
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
+
+  private
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-
 end
