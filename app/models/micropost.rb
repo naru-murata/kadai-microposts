ea4b8ddf653e5adfcc36401a6e2db6a763edc86e
naru.murata <ubuntu@ip-172-31-20-62.ap-northeast-1.compute.internal>
2021-03-01 04:47:39 +0000

    favorite

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   
+  has_many :favorites, dependent: :destroy
+  has_many :likers, through: :favorites, source: :micropost
+  
   validates :content, presence: true, length: { maximum: 255 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,31 @@ class User < ApplicationRecord
     has_secure_password
     
     has_many :microposts
+    
+    has_many :favorites, dependent: :destroy
+    has_many :likes, through: :favorites, source: :micropost
+    
     has_many :relationships
     has_many :followings, through: :relationships, source: :follow
     has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
     has_many :followers, through: :reverses_of_relationship, source: :user
+    
+    def favorite(micropost)
+        self.favorites.find_or_create_by(micropost_id: micropost.id)
+    end
+    
+    def unfavorite(micropost)
+        favorite = self.favorites.find_by(micropost_id: micropost.id)
+        favorite.destroy if favorite
+    end
+    
+    def likes?(micropost)
+        self.likes.include?(micropost)
+    end
+    
+    def feed_likes
+        Micropost.where(user_id: self.like_ids)
+    end
     
     def follow(other_user)
         unless self == other_user
